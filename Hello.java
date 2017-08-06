2d2e4072cc88a1b120f7a57051655518452ec983
collinsnjau39@gmail.com <collinsnjau39@gmail.com>
2017-08-06 20:13:12 +0300

    I have added code to gather name and age

--- Hello.java
+++ Hello.java
@@ -2,8 +2,12 @@ import java.io.Console;
 public class Hello{
     public static void main(String[] args) {
         Console myConsole =System.console();
-        System.out.println("What is your name?");
-        String name=myConsole.readLine();
-        System.out.println("Hello "+ name);
+        System.out.println("what is your name?");
+        String myName=myConsole.readLine();
+        System.out.println("Which year were you born?");
+        String myYear=myConsole.readLine();
+        Integer myAge= Integer.parseInt(myYear);
+        Integer myActualAge= 2017-myAge;
+        System.out.println("hello my name is: " + myName + " and i am " + myActualAge +" years old");
     }
 }
